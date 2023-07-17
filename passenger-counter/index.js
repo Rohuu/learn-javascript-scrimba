ad9879a139875c6a26ecd9bb0eccab9f3376a832
Rohuu <rohuu99022@gmail.com>
2023-07-17 19:05:45 +0530

    save button and rendering text using .innerText

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -1,4 +1,9 @@
 let countEl = document.getElementById("count-passengers");
+let welcomeEl = document.getElementById("welcome-el");
+let name = "Rohit";
+let greetings = "Welcome back, ";
+welcomeEl.innerText = greetings + name;
+
 let count = 0;
 function increment() {
     count = count + 1;
@@ -8,4 +13,8 @@ function increment() {
 function decrement() {
     count = count - 1;
     countEl.innerText = count;
+}
+
+function save() {
+    console.log(count);
 }
