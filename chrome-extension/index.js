4e81b50652ca46e491d7158461c9873adc414db2
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-08-02 16:33:21 +0530

    alternative of .innerHTML

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -9,5 +9,13 @@ inputBtn.addEventListener("click", function(){
 });
 
 for(let i=0;i< myLeads.length;i++){
-    ulEl.innerHTML += "<li>" +myLeads[i]+ "</li>";
+    // ulEl.innerHTML += "<li>" +myLeads[i]+ "</li>";  
+    // there is an alternative for this innerHTML which has 3 steps:
+    // 1. creating element (here li element)  
+    // 2. set text content 
+    // 3. append to ul  
+
+    let liEl= document.createElement("li");
+    liEl.textContent=myLeads[i];
+    ulEl.append(liEl);
 }
