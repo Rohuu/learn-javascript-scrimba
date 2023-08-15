d1fd41b46e36ea6f72313fcfc84274c333f5876a
Rohit Singh Bhandari <rohuu99022@gmail.com>
2023-08-15 23:54:39 +0530

    using chrome API to get the current tab link

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -3,6 +3,15 @@ const inputBtn=document.getElementById("input-btn");
 const inputEl=document.getElementById("input-el"); 
 const ulEl=document.getElementById("ul-el"); 
 const deleteBtn=document.getElementById("delete-btn");
+const tabBtn=document.getElementById("tab-btn");
+
+tabBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myleads",JSON.stringify(myLeads));
+    render(myLeads); 
+  })
+});
 
 let leadsFromLocalStorage= JSON.parse(localStorage.getItem("myLeads"));
 if(leadsFromLocalStorage){
